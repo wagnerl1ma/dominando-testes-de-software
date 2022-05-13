45a49822155e398af38575ea0afa1a4b88909fd2
Wagner Lima <wagner.araujo92@outlook.com>
2022-05-13 20:58:09 -0300

    Asserções (Assert)

---
+++ TesteDeSoftware/01 - Testes de Unidade/Demo.Tests/02 - AssertStringsTests.cs
@@ -0,0 +1,92 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertStringsTests
+    {
+
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Wagner", "Lima");
+
+            //Assert
+            Assert.Equal("Wagner Lima", nomeCompleto);
+
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveIgnorarCase() //Ignorando Letras Maiusculas ou Minusculas
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Wagner", "Lima");
+
+            // Assert
+            Assert.Equal("WAGNER LIMA", nomeCompleto, true);
+        }
+
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Wagner", "Lima");
+
+            // Assert
+            Assert.Contains("agne", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom() //Busca as letras do começo da string
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Wagner", "Lima");
+
+            // Assert
+            Assert.StartsWith("Wag", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveAcabarCom() //Busca as letras do fim da string
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Wagner", "Lima");
+
+            // Assert
+            Assert.EndsWith("ima", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_ValidarExpressaoRegular()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Wagner", "Lima");
+
+            // Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+    }
+}
---
+++ TesteDeSoftware/01 - Testes de Unidade/Demo.Tests/07 - AssertingCollectionsTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertingCollectionsTests
+    {
+        [Fact]
+        public void Funcionario_Habilidades_NaoDevePossuirHabilidadesVazias()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Wagner", 10000);
+
+            // Assert
+            Assert.All(funcionario.Habilidades, habilidade => Assert.False(string.IsNullOrWhiteSpace(habilidade)));
+        }
+
+        [Fact]
+        public void Funcionario_Habilidades_JuniorDevePossuirHabilidadeBasica()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Wagner", 1000);
+
+            // Assert
+            Assert.Contains("OOP", funcionario.Habilidades);
+        }
+
+
+        [Fact]
+        public void Funcionario_Habilidades_JuniorNaoDevePossuirHabilidadeAvancada()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Wagner", 1000);
+
+            // Assert
+            Assert.DoesNotContain("Microservices", funcionario.Habilidades);
+        }
+
+
+        [Fact]
+        public void Funcionario_Habilidades_SeniorDevePossuirTodasHabilidades()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Wagner", 15000);
+
+            var habilidadesBasicas = new[]
+            {
+                "Lógica de Programação",
+                "OOP",
+                "Testes",
+                "Microservices"
+            };
+
+
+            // Assert
+            //Neste teste está comparando duas listas
+            Assert.Equal(habilidadesBasicas, funcionario.Habilidades);
+        }
+    }
+}
