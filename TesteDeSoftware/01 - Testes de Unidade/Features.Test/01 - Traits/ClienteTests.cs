66d4313d84647d5dbce6007f47bafc38d30d12af
Wagner Lima <wagner.araujo92@outlook.com>
2022-05-14 09:36:50 -0300

    Utilizando Traits

---
+++ TesteDeSoftware/01 - Testes de Unidade/Features.Test/01 - Traits/ClienteTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")] //nome do teste
+        [Trait("Categoria", "Cliente Trait Testes")] // cria uma categoria para exibiçao dos testes nas configuracoes
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Wagner",
+                "Lima",
+                DateTime.Now.AddYears(-30),
+                "wagner@wagner.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "wa2wa.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
