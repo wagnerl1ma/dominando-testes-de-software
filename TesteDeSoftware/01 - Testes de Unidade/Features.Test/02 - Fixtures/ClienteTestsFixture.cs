1aa16915f2985948ed238b92a84ed3636349594d
Wagner Lima <wagner.araujo92@outlook.com>
2022-05-14 10:09:41 -0300

    Utilizando Fixtures

---
+++ TesteDeSoftware/01 - Testes de Unidade/Features.Test/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests
+{
+    //Collection Fixture é usado para sempre usar uma mesma instancia de um obj nos testes.
+
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    { }
+
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Wagner",
+                "Lima",
+                DateTime.Now.AddYears(-30),
+                "wagner@wagner.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "wag2wagner.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
