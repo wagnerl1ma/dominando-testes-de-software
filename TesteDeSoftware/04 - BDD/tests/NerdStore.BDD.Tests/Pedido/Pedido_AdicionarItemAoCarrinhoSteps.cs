047fd8dc659eae4612a886d6d085c554cf8fa7be
Wagner Lima <wagner.araujo92@outlook.com>
2022-06-23 11:53:44 -0300

    config para testes BDD

---
+++ TesteDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Pedido/Pedido_AdicionarItemAoCarrinhoSteps.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace NerdStore.BDD.Tests.Pedido
+{
+    [Binding]
+    public class Pedido_AdicionarItemAoCarrinhoSteps
+    {
+        [Given(@"Que um produto esteja na vitrine")]
+        public void DadoQueUmProdutoEstejaNaVitrine()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [Given(@"Esteja disponivel no estoque")]
+        public void DadoEstejaDisponivelNoEstoque()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [Given(@"O usuario esteja logado")]
+        public void DadoOUsuarioEstejaLogado()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [When(@"O usuário adicionar uma unidade ao carrinho")]
+        public void QuandoOUsuarioAdicionarUmaUnidadeAoCarrinho()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [Then(@"O usuário será redirecionado ao resumo da compra")]
+        public void EntaoOUsuarioSeraRedirecionadoAoResumoDaCompra()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [Then(@"O valor total do pedido será exatamente o valor do item adicionado")]
+        public void EntaoOValorTotalDoPedidoSeraExatamenteOValorDoItemAdicionado()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [When(@"O usuário adicionar um item acima da quantidade máxima permitida")]
+        public void QuandoOUsuarioAdicionarUmItemAcimaDaQuantidadeMaximaPermitida()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [Then(@"Receberá uma mensagem de erro mencionando que foi ultrapassada a quantidade limite")]
+        public void EntaoReceberaUmaMensagemDeErroMencionandoQueFoiUltrapassadaAQuantidadeLimite()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [Given(@"O mesmo produto já tenha sido adicionado ao carrinho anteriormente")]
+        public void DadoOMesmoProdutoJaTenhaSidoAdicionadoAoCarrinhoAnteriormente()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [Then(@"A quantidade de itens daquele produto terá sido acrescida em uma unidade a mais")]
+        public void EntaoAQuantidadeDeItensDaqueleProdutoTeraSidoAcrescidaEmUmaUnidadeAMais()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [Then(@"O valor total do pedido será a multiplicação da quantidade de itens pelo valor unitario")]
+        public void EntaoOValorTotalDoPedidoSeraAMultiplicacaoDaQuantidadeDeItensPeloValorUnitario()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }
+
+        [When(@"O usuário adicionar a quantidade máxima permitida ao carrinho")]
+        public void QuandoOUsuarioAdicionarAQuantidadeMaximaPermitidaAoCarrinho()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+        }        
+    }
+}
